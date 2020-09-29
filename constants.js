97d3fff3aeda3222346cf59864e2741b86583527
Anton Bohomaz <bogomaz97@gmail.com>
2020-09-29 15:47:28 +0300

    add labirynth denerator implementration.

---
+++ constants.js
@@ -0,0 +1,4 @@
+const ENTRANCE = 1;
+const PATH = 2;
+const WALL = 3;
+const EXIT = 4;
--- generator.js
+++ generator.js
@@ -1,8 +1,51 @@
-const ENTRANCE = 1;
-const PATH = 2;
-const WALL = 3;
-const EXIT = 4;
+function getRandomCell(rowsNumber, columnsNumber) {
+  let row = Math.floor(Math.random() * rowsNumber);
+  row === rowsNumber ? row - 1 : row;
 
-function generateLabyrinth(n, m) {
-  
+  let column = Math.floor(Math.random() * columnsNumber);
+  column === columnsNumber ? column - 1 : column;
+
+  return { row, column };
 }
+
+function getPathOrWall() {
+  return Math.random() > 0.5 ? PATH : WALL;
+}
+
+function generateLabyrinth(rowsNumber, columnsNumber) {
+  const labyrinth = [];
+  for (let raw = 0; raw < rowsNumber; raw++) {
+    labyrinth[raw] = [];
+    for (let column = 0; column < columnsNumber; column++) {
+      labyrinth[raw][column] = 0;
+    }
+  }
+
+  const randomCellForEntrance = getRandomCell(rowsNumber, columnsNumber);
+  labyrinth[randomCellForEntrance.row][randomCellForEntrance.column] = ENTRANCE;
+
+  let randomCellForExit
+  do {
+    randomCellForExit = getRandomCell(rowsNumber, columnsNumber);
+  } while (
+    randomCellForExit.row === randomCellForEntrance.row 
+    && randomCellForExit.column === randomCellForEntrance.column
+  );
+
+  labyrinth[randomCellForExit.row][randomCellForExit.column] = EXIT;
+
+  for (let raw = 0; raw < rowsNumber; raw++) {
+    for (let column = 0; column < columnsNumber; column++) {
+      if (
+        labyrinth[raw][column] === 0
+        ) {
+        labyrinth[raw][column] = getPathOrWall();
+      }
+    }
+  }
+
+  return labyrinth
+}
+
+const lab = generateLabyrinth(2, 3);
+console.log(lab);
