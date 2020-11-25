4bd8c94bf8d3fb0a13236ff7ad707b57d77c61cf
Anton <anton@Users-MacBook-Pro.local>
2020-11-25 13:35:52 +0200

    [FE] add webpack

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,17 @@
 import { classMap, ROW_ELEMENT_CLASS } from "./constants.js";
 
 export function drawMaze(maze) {
+  document.body.style.background = 'red';
   maze.forEach(row => {
     const rowElement = document.createElement('div');
     rowElement.className = ROW_ELEMENT_CLASS;
-    document.querySelector('#maze-container').append(rowElement);
+    let container = document.querySelector('#maze-container');
+    if (!container) {
+      container = document.createElement('div');
+      container.id = 'maze-container';
+      document.body.append(container)
+    }
+    container.append(rowElement);
     row.forEach(cellType => {
       const div = document.createElement('div');
       div.className = classMap[cellType];
